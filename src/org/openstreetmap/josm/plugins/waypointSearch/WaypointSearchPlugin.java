0823a7b256fd5aa23063ff525a691d10ec7dbed9
don-vip <vincent.privat@gmail.com>
2012-08-24 11:41:33 +0000

    [josm_plugins] See #josm7980 - Fix memory leaks in alignways, fixAdresses, measurement, routing, turnlanes and waypoint_search plugins

--- src/org/openstreetmap/josm/plugins/waypointSearch/WaypointSearchPlugin.java
+++ src/org/openstreetmap/josm/plugins/waypointSearch/WaypointSearchPlugin.java
@@ -1,12 +1,13 @@
 package org.openstreetmap.josm.plugins.waypointSearch;
 
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import org.openstreetmap.josm.gui.MapFrame;
+import org.openstreetmap.josm.gui.MapView;
+import org.openstreetmap.josm.gui.MapView.LayerChangeListener;
+import org.openstreetmap.josm.gui.layer.Layer;
 import org.openstreetmap.josm.plugins.Plugin;
 import org.openstreetmap.josm.plugins.PluginInformation;
-import org.openstreetmap.josm.Main;
-import org.openstreetmap.josm.gui.MapView;
-import org.openstreetmap.josm.gui.layer.Layer;
-import org.openstreetmap.josm.gui.MapView.LayerChangeListener;
-import static org.openstreetmap.josm.tools.I18n.tr;
 
 public class WaypointSearchPlugin extends Plugin implements LayerChangeListener  {
     /**
@@ -14,8 +15,8 @@ public class WaypointSearchPlugin extends Plugin implements LayerChangeListener 
     *
     * @param info  information about the plugin and its local installation    
     */
-    private Engine engine = new Engine();
-    private SelectWaypointDialog waypointDialog = new SelectWaypointDialog(tr("Waypoint search"), "ToolbarIcon", tr("Search after waypoint. Click and move the map view to the waypoint."), null, 100);
+    private final Engine engine = new Engine();
+    private SelectWaypointDialog waypointDialog;
     
     public WaypointSearchPlugin(PluginInformation info) {
        super(info);
@@ -23,62 +24,31 @@ public class WaypointSearchPlugin extends Plugin implements LayerChangeListener 
     }
      
     @Override
-    public void activeLayerChange(Layer oldLayer, Layer newLayer) {
-        // TODO Auto-generated method stub
-        
+    public void mapFrameInitialized(MapFrame oldFrame, MapFrame newFrame) {
+        if (newFrame != null) {
+            newFrame.addToggleDialog(waypointDialog = new SelectWaypointDialog(
+                    tr("Waypoint search"), "ToolbarIcon", tr("Search after waypoint. Click and move the map view to the waypoint."), null, 100));
+        } else {
+            waypointDialog = null;
+        }
     }
 
+    @Override
+    public void activeLayerChange(Layer oldLayer, Layer newLayer) {
+    }
     
     @Override
     public void layerAdded(Layer newLayer) {
-        //add dialog
-        if (Main.map.getToggleDialog(SelectWaypointDialog.class)==null) {
-            Main.map.addToggleDialog(waypointDialog);
-        }
-        //update search
-        if (engine.gpxLayersExist()) {
+        // update search
+        if (waypointDialog != null && engine.gpxLayersExist()) {
             waypointDialog.updateSearchResults();
         }
     }
-
-
 
     @Override
     public void layerRemoved(Layer oldLayer) {
-        if (!engine.gpxLayersExist()) {
+        if (waypointDialog != null && !engine.gpxLayersExist()) {
             waypointDialog.updateSearchResults();
         }   
     }
- 
 }
-
-
-
-    
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-
-
-
-
-
-
